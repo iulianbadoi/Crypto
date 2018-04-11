a6cb9c490a1116e3a8f037b3b4cbef44086f43a6
oshapio <643050@gmail.com>
2018-04-11 19:46:54 +0300

    Pollard's rho fixed

--- findInverse.m
+++ findInverse.m
@@ -1,6 +1,10 @@
 function [ inverse ] = findInverse( a, N)
    % Find inverse of a modulo N, i.e finds x in a * x = 1 MOD N 
    [gcd, x, y] = extendedEuclid(a, N);
-   inverse = mod((x + N), N);
+   if gcd ~= 1
+       inverse = -1;
+   else 
+    inverse = mod((x + N), N);
+   end
 end
 
